1526d0c86e0428ca9a2ee2ca0409ad1e6b699702
JuliaVal821 <julia.kolgatina@gmail.com>
2021-03-05 23:16:35 -0500

    Returned their respective ages now as [humanYears,catYears,dogYears]

---
+++ Cat years, Dog years.js
@@ -0,0 +1,7 @@
+https://www.codewars.com/kata/5a6663e9fd56cb5ab800008b/train/javascript
+
+    var humanYearsCatYearsDogYears = function(humanYears) {
+        if(humanYears === 1) return [1, 15, 15];
+        if(humanYears === 2) return [2, 24, 24];
+        return [humanYears, 24 + 4 * (humanYears - 2), 24 + 5 * (humanYears - 2)];
+    }
